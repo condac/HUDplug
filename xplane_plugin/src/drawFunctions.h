8f080a8344f01b40064eac7715b2b07150978b9d
condac <burns.nu@gmail.com>
2021-11-24 10:59:36 +0100

    maxfunktion

--- xplane_plugin/src/drawFunctions.h
+++ xplane_plugin/src/drawFunctions.h
@@ -32,4 +32,11 @@ void CalculateColors();
 float to_radians(float degrees);
 float to_degrees(float radians);
 
+#define max(a, b)                                                                                                                                                                  \
+    ({                                                                                                                                                                             \
+        __typeof__(a) _a = (a);                                                                                                                                                    \
+        __typeof__(b) _b = (b);                                                                                                                                                    \
+        _a > _b ? _a : _b;                                                                                                                                                         \
+    })
+
 #endif
